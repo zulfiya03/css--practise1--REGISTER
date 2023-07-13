0cb2d012c3195439fedeebbbc8222a5f2c94cc8c
Zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-13 22:45:41 +0500

    next commit

---
+++ page.register/script.js
@@ -0,0 +1,45 @@
+const nameInput = document.getElementById('name')
+const usernameInput = document.getElementById('username')
+const emailInput = document.getElementById('email')
+const numberInput = document.getElementById('number')
+const passwordInput = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('confirmpassword')
+const registerBtn = document.getElementById('register')
+
+let nameM = ['Asadbek', 'Zulfiya']
+let usernameM = ['Xudarganov', 'Abdullaeva']
+let emailM = ['hgcujhikj', '18tuvuhbijn']
+let numberM = ['8999999', '18898989898']
+let passwordM = ['hgcujhikj', '18tuvuhbijn']
+let confirmpasswordM = ['hgcujhikj', '18tuvuhbijn']
+
+table()
+
+registerBtn.onclick = () => {
+    let name = nameInput.value
+    let username = usernameInput.value
+    let email= usernameInput.value
+    let number = usernameInput.value
+    let password = usernameInput.value
+    let confirmpassword = ageInput.value
+
+    nameM.push(name)
+    usernameM.push(username)
+    emailM.push(email)
+    numberM.push(number)
+    passwordM.push(passworda)
+    confirmpasswordM.push(confirmpassword)
+
+    list.innerHTML = ''
+    .
+    table()
+
+    firstInput.value = ""
+    lastInput.value = ""
+    ageInput.value = ""
+}
+
+//registerga a href qo`yish kk registwrni bosgandan kn next page ga
+//o`tishi kerek. va keyyingi sahifada list/jadval bo`lishi kerak
+//bu jadvalda regiterdagi ma`lumotlar jamlangan bo`lishi shart! 
+
